236cf858e5138ca311ad1253c68168115b271d03
Imoze <game-f-90@mail.ru>
2025-03-27 01:17:22 +0300

    Main page
    added main page

--- web_app/main.py
+++ web_app/main.py
@@ -1,19 +1,27 @@
+import os
+
 from flask import Flask, render_template, redirect, request, url_for
 
 from db_handlers import db_login, db_register, getClientData, setClientData
+from symmetric_crypto import simple_encrypt, simple_decrypt
 
 app = Flask(__name__)
 
+KEY = os.getenv("ENCR_KEY")
 
 @app.route('/')
 def index():
-    return redirect('/login')
+    return redirect('/mainpage')
 
 
 
-@app.route('/mainpage')
+@app.route('/mainpage', methods=['GET', 'POST'])
 def mainpage():
-    return "main page"
+    if request.method == 'POST':
+        print('post')
+        return redirect('models')
+    else:
+        return render_template('main.html')
 
 
 
@@ -25,7 +33,7 @@ def login():
         if user == 0:
             return render_template('login.html', error='Worng login or password')
         else:
-            return redirect(url_for('profile',user=user))
+            return redirect(url_for('profile', login=simple_encrypt(login, KEY), password=simple_encrypt(password, KEY)))
     else:
         return render_template('login.html')
     
@@ -38,7 +46,7 @@ def signup():
         if code == 101:
             return render_template('signup.html', error='Login already taken')
         else:
-            return redirect(url_for('profileEdit', user=db_login(login, password)))
+            return redirect(url_for('profileEdit', login=simple_encrypt(login, KEY), password=simple_encrypt(password, KEY)))
     else:
         return render_template('signup.html')
 
@@ -46,10 +54,15 @@ def signup():
 @app.route('/profile', methods=['GET', 'POST'])
 def profile():
     if request.method == 'POST':
-        if request.form['edit-btn'] == 'edit':
-            return redirect(url_for('profileEdit', user=request.args.get('user')))
+        if request.form['action'] == 'edit':
+            return redirect(url_for('profileEdit',
+                                    login=request.args.get('login'),
+                                    password=request.args.get('password')))
+        else:
+            return redirect(url_for('mainpage', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        cl_data = getClientData(request.args.get('user'))
+        cl_data = getClientData(db_login(simple_decrypt(request.args.get('login'), KEY),
+                                         simple_decrypt(request.args.get('password'), KEY)))
         return render_template('profile.html', first_name=cl_data['Firstname'], last_name=cl_data['Lastname'], birth_date=cl_data['BirthDate'],
                                 phone=cl_data['Phone'], email=cl_data['Email'], personal_discount=cl_data['PersonalDiscount'], delivery_adress=cl_data['DeliveryAdress'])
 
@@ -58,14 +71,18 @@ def profile():
 def profileEdit():
     if request.method == 'POST':
         if request.form['action'] == 'cancel':
-            return redirect(url_for('profile', user=request.args.get('user')))
+            return redirect(url_for('profile',
+                                    login=request.args.get('login'),
+                                    password=request.args.get('password')))
         else:
-            newData = (request.args.get('user'), request.form['firstName'], request.form['lastName'], request.form['birthDate'],
+            newData = (db_login(simple_decrypt(request.args.get('login'), KEY), simple_decrypt(request.args.get('password'), KEY)),
+                       request.form['firstName'], request.form['lastName'], request.form['birthDate'],
                        request.form['phone'], request.form['email'], request.form['address'])
             setClientData(newData)
-            return redirect(url_for('profile', user=request.args.get('user')))
+            return redirect(url_for('profile', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        cl_data = getClientData(request.args.get('user'))
+        cl_data = getClientData(db_login(simple_decrypt(request.args.get('login'), KEY),
+                                         simple_decrypt(request.args.get('password'), KEY)))
         return render_template('profile_edit.html', user=cl_data)
 
 
