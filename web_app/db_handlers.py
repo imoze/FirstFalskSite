f00e0dbf81b192f41e5879b3a18dda9416ecb3f5
Imoze <game-f-90@mail.ru>
2025-03-30 19:55:39 +0300

    Fully functional site
    Still need to structirise project folder

--- web_app/db_handlers.py
+++ web_app/db_handlers.py
@@ -5,7 +5,7 @@ import psycopg2 as pg
 DB_PASS = os.getenv("INJ_PASS")
 
 def db_login(login, password):
-    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='injection')
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
 
     cursor = connection.cursor()
 
@@ -16,7 +16,7 @@ def db_login(login, password):
     return res[0]
 
 def db_register(login, password):
-    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='injection')
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
     cursor = connection.cursor()
 
     cursor.callproc('registration', (login, password))
@@ -27,7 +27,7 @@ def db_register(login, password):
     return res[0]
 
 def getClientData(user):
-    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='injection')
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
     cursor = connection.cursor()
 
     cursor.callproc('getClientData', (user,))
@@ -47,7 +47,7 @@ def getClientData(user):
 
 
 def setClientData(dataPull):
-    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='injection')
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
     cursor = connection.cursor()
 
     cursor.callproc('setClientData', (*dataPull,))
@@ -55,4 +55,49 @@ def setClientData(dataPull):
     connection.commit()
     cursor.close()
     connection.close()
+    return res
+
+def orderSum(data):
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
+    cursor = connection.cursor()
+
+    cursor.callproc('summ_for_order', (*data,))
+    res = cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return res
+
+def placeOrder(data):
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
+    cursor = connection.cursor()
+
+    cursor.callproc('place_order', (*data,))
+    res = cursor.fetchone()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    return res
+
+def getOrders(user):
+    connection = pg.connect(user="postgres", password=DB_PASS, host="127.0.0.1", port="5432", database='SiteDB')
+    cursor = connection.cursor()
+
+    cursor.callproc('getOrders', (user,))
+    res = cursor.fetchall()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    res = [
+        {
+            'id': order[0],
+            'model': order[1],
+            'quantity': order[2],
+            'order_date': order[3],
+            'delivery_date': order[4],
+            'total_amount': order[5],
+            'completed': order[6]
+        }
+        for order in res 
+    ]
     return res
--- web_app/main.py
+++ web_app/main.py
@@ -1,9 +1,9 @@
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import Flask, render_template, redirect, request, url_for
 
-from db_handlers import db_login, db_register, getClientData, setClientData
+from db_handlers import db_login, db_register, getClientData, setClientData, placeOrder, orderSum, getOrders
 from symmetric_crypto import simple_encrypt, simple_decrypt
 
 app = Flask(__name__)
@@ -19,77 +19,128 @@ def index():
 @app.route('/mainpage', methods=['GET', 'POST'])
 def mainpage():
     if request.method == 'POST':
-        print('post')
-        return redirect('models')
+        return redirect(url_for('models', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('main.html')
+        return render_template('main.html', login=request.args.get('login'), password=request.args.get('password'))
     
 
 @app.route('/models', methods=['GET', 'POST'])
 def models():
     if request.method == 'POST':
         if request.form['model'] == 'SF90':
-            return redirect('models/SF90')
+            return redirect(url_for('sf90', login=request.args.get('login'), password=request.args.get('password')))
         elif request.form['model'] == 'Roma':
-            return redirect('models/Roma')
+            return redirect(url_for('roma', login=request.args.get('login'), password=request.args.get('password')))
         elif request.form['model'] == '296':
-            return redirect('models/296')
+            return redirect(url_for('f296', login=request.args.get('login'), password=request.args.get('password')))
         elif request.form['model'] == 'Daytona SP3':
-            return redirect('models/Daytona SP3')
+            return redirect(url_for('daytona', login=request.args.get('login'), password=request.args.get('password')))
         elif request.form['model'] == '12Cilindri':
-            return redirect('models/12Cilindri')
+            return redirect(url_for('cilindri', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('models.html')
+        return render_template('models.html', login=request.args.get('login'), password=request.args.get('password'))
 
 
 @app.route('/models/SF90',  methods=['GET', 'POST'])
 def sf90():
     if request.method == 'POST':
-        return redirect(url_for('buy', model='SF 90 Stradale XX'))
+        return redirect(url_for('buy', model='SF90 Stradale XX', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('sf90.html')
+        return render_template('sf90.html', login=request.args.get('login'), password=request.args.get('password'))
 
 @app.route('/models/Roma',  methods=['GET', 'POST'])
 def roma():
     if request.method == 'POST':
-        return redirect(url_for('buy', model='Roma'))
+        return redirect(url_for('buy', model='Roma', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('roma.html')
+        return render_template('roma.html', login=request.args.get('login'), password=request.args.get('password'))
 
 @app.route('/models/296',  methods=['GET', 'POST'])
 def f296():
     if request.method == 'POST':
-        return redirect(url_for('buy', model='296'))
+        return redirect(url_for('buy', model='296', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('296.html')
+        return render_template('296.html', login=request.args.get('login'), password=request.args.get('password'))
 
 @app.route('/models/Daytona SP3',  methods=['GET', 'POST'])
 def daytona():
     if request.method == 'POST':
-        return redirect(url_for('buy', model='Daytona SP3'))
+        return redirect(url_for('buy', model='Daytona SP3', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('daytonasp3.html')
+        return render_template('daytonasp3.html', login=request.args.get('login'), password=request.args.get('password'))
 
 @app.route('/models/12Cilindri',  methods=['GET', 'POST'])
 def cilindri():
     if request.method == 'POST':
-        return redirect(url_for('buy', model='12Cilindri'))
+        return redirect(url_for('buy', model='12Cilindri', login=request.args.get('login'), password=request.args.get('password')))
     else:
-        return render_template('12cilindri.html')
+        return render_template('12cilindri.html', login=request.args.get('login'), password=request.args.get('password'))
     
 
 
 @app.route('/buy',  methods=['GET', 'POST'])
 def buy():
     if request.method == 'POST':
-        print(f"Now: {datetime.now()} date:{datetime.strptime(request.form['delivery-date'], '%Y-%m-%d')}")
-        if datetime.now() < datetime.strptime(request.form['delivery-date'], '%Y-%m-%d'):
-            return "Succesfull buy"
+        notBefore = datetime.now() + timedelta(days=1) < datetime.strptime(f'{request.form["delivery-date"]} {request.form["delivery-time"]}', '%Y-%m-%d %H:%M:%S')
+        notToLong =  datetime.strptime(request.form['delivery-date'], '%Y-%m-%d') < datetime.now() + timedelta(days=365)
+        if notBefore and notToLong:
+            res = orderSum((db_login(simple_decrypt(request.args.get('login'), KEY), simple_decrypt(request.args.get('password'), KEY)),
+                            request.args['model'], request.form['quantity']))[0]
+            if res == 400:
+                return render_template('buy.html', model=request.args['model'], error='This model with this amount is anavalible', login=request.args.get('login'), password=request.args.get('password'))
+            else:
+                return redirect(url_for('payment', login=request.args.get('login'), password=request.args.get('password'),
+                                        model=request.args['model'], quantity=request.form['quantity'], sum=res,
+                                        del_date=f'{request.form["delivery-date"]} {request.form["delivery-time"]}'))
         else:
-            return render_template('buy.html', model=request.args['model'], error='Incorrect date')
+            return render_template('buy.html', model=request.args['model'], error='Incorrect date', login=request.args.get('login'), password=request.args.get('password'))
     else:
-        return render_template('buy.html', model=request.args['model'])
+        return render_template('buy.html', model=request.args['model'], login=request.args.get('login'), password=request.args.get('password'))
+    
 
+@app.route('/payment', methods=['GET', 'POST'])
+def payment():
+    if request.method == 'POST':
+        if request.form['action'] == 'pay':
+            placeOrder((db_login(simple_decrypt(request.args.get('login'), KEY),
+                                 simple_decrypt(request.args.get('password'), KEY)),
+                        datetime.strftime(datetime.now(),"%d-%m-%Y %H:%M:%S"),
+                        datetime.strftime(datetime.strptime(request.args['del_date'], "%Y-%m-%d %H:%M:%S"), "%d-%m-%Y %H:%M:%S"),
+                        request.args['model'],
+                        request.args['quantity']))
+            return redirect(url_for('success', login=request.args.get('login'), password=request.args.get('password'),
+                            model=request.args['model'], quantity=request.args['quantity'], del_date=request.args['del_date'], sum=request.args['sum']))
+        else:
+            return redirect(url_for('models', login=request.args.get('login'), password=request.args.get('password')))
+    else:
+        return render_template('payment.html', model=request.args['model'], 
+                               quantity=request.args['quantity'],
+                               delivery_date=request.args['del_date'],
+                               total_amount=request.args['sum'],
+                               login=request.args.get('login'), password=request.args.get('password'))
+
+
+@app.route('/success', methods=['GET', 'POST'])
+def success():
+    if request.method == 'POST':
+        if request.form['action'] == 'hist':
+            return redirect(url_for('history', login=request.args.get('login'), password=request.args.get('password')))
+        else:
+            return redirect(url_for('mainpage', login=request.args.get('login'), password=request.args.get('password')))
+    else:
+        return render_template('success_order.html', model=request.args['model'], quantity=request.args['quantity'],
+                               delivery_date=request.args['del_date'], total_amount=request.args['sum'])
+    
+
+@app.route('/history', methods=['GET','POST'])
+def history():
+    if request.method == 'POST':
+        return 123
+    else:
+        orders = getOrders(db_login(simple_decrypt(request.args.get('login'), KEY),
+                                 simple_decrypt(request.args.get('password'), KEY)))
+        
+        return render_template('history.html', orders=orders, login=request.args.get('login'), password=request.args.get('password'))
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -125,8 +176,12 @@ def profile():
             return redirect(url_for('profileEdit',
                                     login=request.args.get('login'),
                                     password=request.args.get('password')))
-        else:
+        elif request.form['action'] == 'hist':
+            return redirect(url_for('history', login=request.args.get('login'), password=request.args.get('password')))
+        elif request.form['action'] == 'main':
             return redirect(url_for('mainpage', login=request.args.get('login'), password=request.args.get('password')))
+        else:
+            return redirect('/login')
     else:
         cl_data = getClientData(db_login(simple_decrypt(request.args.get('login'), KEY),
                                          simple_decrypt(request.args.get('password'), KEY)))
